18b05edcf7c25897b5d7b77dc435fef4777eaed8
vaahtopahvi <kermakahvi@hotmail.com>
2024-12-24 02:41:45 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./card";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden bg-red flex items-center justify-center">
+      <Card />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Img1 from './assets/react.svg';
+
+const Card = () => {
+
+    return (
+        <div
+        >
+
+        </div>
+    )
+}
+
+export default Card;
