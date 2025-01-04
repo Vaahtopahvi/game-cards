172165c226fff0b0872d01d66281a9486a7fa913
vaahtopahvi <kermakahvi@hotmail.com>
2025-01-04 02:42:59 +0200

    update code and add first card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Card from "./card";
+import Card from "./components/Card";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen overflow-hidden bg-red flex items-center justify-center">
+    <div className="w-screen h-screen overflow-hidden bg-black flex items-center justify-center">
       <Card />
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
