bdbc9de24c0588e2bbedd35e6da89d1b3353b5c0
vaahtopahvi <kermakahvi@hotmail.com>
2025-01-06 19:53:11 +0200

    update and add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
 import Card from "./components/Card";
+import img1 from "./assets/image1.png";
+import img2 from "./assets/image2.png";
+import img3 from "./assets/image3.png";
+import img4 from "./assets/image4.png";
+import img5 from "./assets/image5.png";
+import CardStacker from "./components/CardStacker";
 
 const App = () => {
+  const data = [
+    {
+      name: "Wise Old Man",
+      subtitle: "Resident of Gielinor",
+      rating: "4.6",
+      background: { top: "#2d3142", bottom: "#c5a3cf" },
+      image: img1,
+    },
+    {
+      name: "The Binding of Isaac",
+      subtitle: "Isaac, the basement Boy",
+      rating: "5.0",
+      background: { top: "#1B1B1B", bottom: "#632842" },
+      image: img2,
+    },
+    {
+      name: "God Of War",
+      subtitle: "Furious Kratos",
+      rating: "4.3",
+      background: { top: "#4b1e11", bottom: "#f17b3e" },
+      image: img3,
+    },
+    {
+      name: "Silent Hill 2",
+      subtitle: "Pyramid Head, scary",
+      rating: "4.8",
+      background: { top: "#6B2F2F", bottom: "#7F4E33" },
+      image: img4,
+    },
+    {
+      name: "Battlefield 4",
+      subtitle: "One of the great old modern battlefields",
+      rating: "4.9",
+      background: { top: "#0b132b", bottom: "#f28a30" },
+      image: img5,
+    },
+  ];
+
   return (
     <div className="w-screen h-screen overflow-hidden bg-black flex items-center justify-center">
-      <Card />
+      <CardStacker data={data}></CardStacker>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,24 @@
-// import React from 'react';
-import img1 from "../assets/image1.png";
+import React from "react";
 import Bg from "../assets/bg.png";
 
-const Card = () => {
+const Card = ({
+  name,
+  subtitle,
+  rating,
+  background,
+  image,
+  className = "",
+}) => {
+  const { top, bottom } = background;
+
   return (
     <div
-      className="flex flex-col justify-center p-5 rounded-[10%] relative text-white overflow-hidden border-[1px] border-white/75"
+      className={
+        "card flex flex-col justify-center p-5 rounded-[10%] relative text-white overflow-hidden h-fit w-fit z-10 hover:-translate-y-10 transition-all duration-300 " +
+        className
+      } // Added className prop so they will stack on top of each other
       style={{
-        background: "linear-gradient(to bottom, #51D1F7, #5B8FEF",
+        background: `linear-gradient(to bottom, ${top}, ${bottom})`,
       }}
     >
       <div className="inset-0 absolute z-0">
@@ -19,8 +30,8 @@ const Card = () => {
       </div>
       <div className="flex items-center justify-between z-10">
         <div>
-          <h2 className="text-2xl font-bold mb-0.5">Wise Old Man</h2>
-          <p className="text-xs opacity-70">Resident of Gielinor</p>
+          <h2 className="text-2xl font-bold mb-0.5">{name}</h2>
+          <p className="text-xs opacity-70">{subtitle}</p>
         </div>
         <div className="bg-[rgba(0,0,0,0.4)] flex items-center pl-1 pr-2 py-1 rounded-3xl gap-2">
           <div className="icon h-[25px] w-[25px] rounded-full grid place-content-center bg-white">
@@ -34,13 +45,13 @@ const Card = () => {
               <path d="M232,64H208V48a8,8,0,0,0-8-8H56a8,8,0,0,0-8,8V64H24A16,16,0,0,0,8,80V96a40,40,0,0,0,40,40h3.65A80.13,80.13,0,0,0,120,191.61V216H96a8,8,0,0,0,0,16h64a8,8,0,0,0,0-16H136V191.58c31.94-3.23,58.44-25.64,68.08-55.58H208a40,40,0,0,0,40-40V80A16,16,0,0,0,232,64ZM48,120A24,24,0,0,1,24,96V80H48v32q0,4,.39,8ZM232,96a24,24,0,0,1-24,24h-.5a81.81,81.81,0,0,0,.5-8.9V80h24Z"></path>
             </svg>
           </div>
-          <p className="text-xs">5.0</p>
+          <p className="text-xs">{rating}</p>
         </div>
       </div>
       <img
-        src={img1}
-        alt="Kirby"
-        className="w-72 h-72 object-contain mt-5 drop-shadow-[0_2px_2px_rgba(0,0,0,0.2)] mx-5 mb-7"
+        src={image}
+        alt={name}
+        className="w-64 h-64 object-contain mt-5 drop-shadow-[0_2px_2px_rgba(0,0,0,0.2)] mx-5 mb-7"
       />
     </div>
   );
